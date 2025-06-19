a56d3d6bf9b57ce8039bef5fefd54ed52326cb5c
joaoeri <211980161+joaoeri@users.noreply.github.com>
2025-06-19 15:36:06 -0300

    first commit

---
+++ node-exporter/install.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+set -euo pipefail
+trap 's=$?; echo >&2 "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+
+# Check if user passed Node Exporter version
+if [[ "$#" -ne 1  ]]; then
+    echo "This scrips requires exactly ONE argument."
+    echo "Usage: ./${0} <NODE_EXPORTER_VERSION>"
+    exit 1
+fi
+
+# Set Node Exporter version
+export VERSION=${1} 
+
+# Gather basic system information
+export OS="$(uname | tr A-Z a-z)" # Operational System
+export ARCH="$(uname -m)" # CPU Architecture
+
+if [[ $ARCH == "x86_64" ]]; then
+    export ARCH="amd64"
+fi
+
+# Download and install Node Exporter Binary
+wget https://github.com/prometheus/node_exporter/releases/download/v${VERSION}/node_exporter-${VERSION}.${OS}-${ARCH}.tar.gz
+tar xvfz node_exporter-*.*-${ARCH}.tar.gz
+cd node_exporter-*.*-${ARCH}
+sudo mv node_exporter /usr/local/bin
+
+# Create node_exporter user with /sbin/nologin shell and no special privileges
+sudo useradd -r -s /sbin/nologin -M node_exporter
+
+# Create systemd service
+sudo cat <<EOF > /etc/systemd/system/node_exporter.service
+[Unit]
+Description=Node Exporter
+After=network.target
+
+[Service]
+User=node_exporter
+ExecStart=/usr/local/bin/node_exporter
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Enable and start systemd service
+sudo systemctl enable --now node_exporter
